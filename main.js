38bf48a546f95fb4558d9ef259c020f804a7abb4
The19s <the19sprime@gmail.com>
2024-11-19 11:05:20 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+// Placeholder for future JavaScript functionality
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+});
