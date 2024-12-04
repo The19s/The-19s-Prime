e7b61518b8a531a5a817e2dcdbfe04359e3c5648
The19s <the19sprime@gmail.com>
2024-12-03 22:10:57 -0500

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-// Placeholder for future JavaScript functionality
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for your message! We will get back to you soon.');
-});
